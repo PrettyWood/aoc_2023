ab4e648a8477330de0dff1af9e38bfd39a6f9bd2
Eric Jolibois <em.jolibois@gmail.com>
2023-12-07 07:51:34 +0100

    simplify part 5

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -7,23 +7,18 @@ use nom::{
     multi::{many1, separated_list1},
     sequence::{preceded, separated_pair, tuple},
 };
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() {
     println!("Part 1: {}", part1(include_str!("input.txt")));
-    // FIXME: Need to rework the whole thing to handle part 2
-    // println!("Part 2: {}", part2(include_str!("input.txt")));
+    println!("Part 2: {}", part2(include_str!("input.txt")));
 }
 
 #[derive(Debug)]
-struct TMap {
-    source: String,
-    destination: String,
-    // (source, destination, range)
-    ranges: Vec<(usize, usize, usize)>,
-}
+struct TMap(Vec<(usize, usize, usize)>);
 
 fn parse_tmap(input: &str) -> nom::IResult<&str, TMap> {
-    let (input, (source, destination)) = separated_pair(alpha1, tag("-to-"), alpha1)(input)?;
+    let (input, (_source, _destination)) = separated_pair(alpha1, tag("-to-"), alpha1)(input)?;
     let (input, _) = tag(" map:")(input)?;
     let (input, _) = newline(input)?;
     let (input, ranges) = separated_list1(
@@ -33,30 +28,13 @@ fn parse_tmap(input: &str) -> nom::IResult<&str, TMap> {
     let ranges = ranges
         .iter()
         .map(|&(d, _, s, _, r)| (s as usize, d as usize, r as usize))
-        .collect::<Vec<_>>();
-    Ok((
-        input,
-        TMap {
-            source: source.to_string(),
-            destination: destination.to_string(),
-            ranges,
-        },
-    ))
-}
-
-impl FromStr for TMap {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        parse_tmap(s)
-            .map(|(_, tmap)| tmap)
-            .map_err(|e| e.to_string())
-    }
+        .collect::<Vec<(usize, usize, usize)>>();
+    Ok((input, TMap(ranges)))
 }
 
 impl TMap {
     fn convert(&self, n: usize) -> usize {
-        self.ranges
+        self.0
             .iter()
             .find(|&&(s, _, r)| (s..s + r).contains(&n))
             .map(|&(s, d, _)| d + (n - s))
@@ -93,18 +71,8 @@ impl FromStr for TGrid {
 }
 
 impl TGrid {
-    fn select_map(&self, source: &str) -> Option<&TMap> {
-        self.maps.iter().find(|m| m.source == source)
-    }
-
     fn find_location(&self, seed: usize) -> usize {
-        let mut map_name = "seed";
-        let mut seed = seed;
-        while let Some(map) = self.select_map(map_name) {
-            map_name = &map.destination;
-            seed = map.convert(seed);
-        }
-        seed
+        self.maps.iter().fold(seed, |seed, map| map.convert(seed))
     }
 }
 
@@ -119,12 +87,15 @@ pub fn part1(input: &str) -> usize {
 
 pub fn part2(input: &str) -> usize {
     let grid = TGrid::from_str(input).unwrap();
-    grid.seeds
+    let seeds_ranges = grid
+        .seeds
         .chunks(2)
-        .flat_map(|chunk| chunk[0]..chunk[0] + chunk[1])
-        .collect::<HashSet<usize>>()
-        .iter()
-        .map(|seed| grid.find_location(*seed))
+        .map(|c| c[0]..c[0] + c[1])
+        .collect::<HashSet<_>>();
+    seeds_ranges
+        .par_iter()
+        .flat_map(|range| range.clone())
+        .map(|seed| grid.find_location(seed))
         .min()
         .unwrap()
 }
@@ -132,62 +103,6 @@ pub fn part2(input: &str) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_map() {
-        let input = r#"seed-to-soil map:
-50 98 2
-52 50 48"#;
-        let map = TMap::from_str(input).unwrap();
-        assert_eq!(map.source, "seed");
-        assert_eq!(map.destination, "soil");
-        assert_eq!(map.convert(98), 50);
-        assert_eq!(map.convert(99), 51);
-        for i in 0..48 {
-            assert_eq!(map.convert(50 + i), 52 + i);
-        }
-        assert_eq!(map.convert(100), 100);
-        assert_eq!(map.convert(10), 10);
-    }
-    #[test]
-    fn test_parse_maps() {
-        let input = r#"seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4"#;
-        let grid = TGrid::from_str(input).unwrap();
-        assert_eq!(grid.seeds, vec![79, 14, 55, 13]);
-        assert_eq!(grid.maps.len(), 7);
-    }
 
     #[test]
     fn test_part1() {
